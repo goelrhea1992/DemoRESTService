5ab2d60d44b1c4511fb5acd18aa7d9ef3aec4d1f
msd2178 <msd2178@columbia.edu>
2015-02-01 14:35:10 -0500

    commented out view statements & display JSON

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //---res.render('index', { title: 'Express' });
+res.setHeader('Content-Type', 'application/json');
+res.end(JSON.stringify({ a: 1 }));
 });
 
 module.exports = router;
