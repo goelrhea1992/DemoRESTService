d96e3b7d1a3d232561e2f862f189830fe060a4bb
Robert Kuykendall <robert@rkuykendall.com>
2015-02-01 14:55:20 -0500

    Improve JSON response.

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //---res.render('index', { title: 'Express' });
-res.setHeader('Content-Type', 'application/json');
-res.end(JSON.stringify({ a: 1 }));
+    res.json({ a: 1 });
 });
 
 module.exports = router;
