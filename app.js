d5562b34b4311f0cf1aadedf9397c3232ee209c6
mahajandiwakar <dm3084@columbia.edu>
2015-02-05 09:49:41 -0500

    Adding support for categories

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var db = mysql.createConnection(config.db);
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var actors = require('./routes/actors');
+var categories = require('./routes/categories');
 var films = require('./routes/films');
 var app = express();
 
@@ -37,6 +38,7 @@ app.use(function(req, res, next){
 app.use('/', routes);
 app.use('/users', users);
 app.use('/actors', actors);
+app.use('/categories', categories);
 app.use('/films', films);
 
 // catch 404 and forward to error handler
